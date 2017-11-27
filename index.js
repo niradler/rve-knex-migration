732f0daf48782e15905962241b73c143778392c2
Nir <niradler55@gmail.com>
2017-11-27 22:34:28 +0200

    add q

--- index.js
+++ index.js
@@ -1,143 +1,112 @@
 #!/usr/bin/env node
 
-// const program = require('commander');
-// const { prompt } = require('inquirer');
+const program = require('commander');
+const {
+    prompt
+} = require('inquirer');
 const Knex = require('knex');
 const gen = require('./gen');
 const fs = require('fs');
 
 var knex;
 var tables;
-async function getSchema(con) {
+
+async function createMigration(con) {
+
     knex = Knex({
         client: 'mysql',
         connection: {
-            host: '127.0.0.1',
-            user: 'root',
-            password: '',
-            database: 'shelfmint_v2_local'
+            host: con.host,
+            user: con.user,
+            password: con.password ? con.password : '',
+            database: con.db
         }
     });
+ 
+    try{
+        var is_con = await knex.raw('select 1+1 as result');
+    }catch(e){
+        console.log('connection error!')
+        process.exit(1)
+    }
+    
 
     let r = await knex.raw('show tables');
-    const tables = r[0].map((t)=>{
-       const props = Object.getOwnPropertyNames(t);
-       return t[props[0]]
+    const tables = r[0].map((t) => {
+        const props = Object.getOwnPropertyNames(t);
+        return t[props[0]]
     })
     //console.log(tables);
-   const describes = [];
-   for (const i in tables) {
-    r = await knex.raw('describe '+ tables[i])
-    describes.push({table:tables[i],cols:r[0]})
-   }
-  // console.log(describes);
-  if (!fs.existsSync('./migrations')){
-    fs.mkdirSync('./migrations');
-}
-  for (let i = 0; i < describes.length; i++) {
-    const file = gen(describes[i].table, describes[i].table, describes[i].cols);
-    
-    fs.writeFile(`./migrations/${parseInt(Math.random() * Math.pow(10, 13) )}_${describes[i].table}.js`, file, function(err) {
-        if(err) {
-            return console.log(err);
+    const describes = [];
+    for (const i in tables) {
+        try{
+            r = await knex.raw('describe ' + tables[i])
+            describes.push({
+                table: tables[i],
+                cols: r[0]
+            })
+        }catch(e){
+            console.log('err',e)
         }
-    });
-      
-  }
- //process.exit();
+     
+    }
+    // console.log(describes);
+    if (!fs.existsSync('./migrations')) {
+        fs.mkdirSync('./migrations');
+    }
+    for (let i = 0; i < describes.length; i++) {
+        const file = gen(describes[i].table, describes[i].table, describes[i].cols);
+
+        fs.writeFile(`./migrations/${parseInt(Math.random() * Math.pow(10, 13) )}_${describes[i].table}.js`, file, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
 }
-getSchema();
-// const questions = [
-//   {
-//     type : 'input',
-//     name : 'firstname',
-//     message : 'Enter firstname ..'
-//   },
-//   {
-//     type : 'input',
-//     name : 'lastname',
-//     message : 'Enter lastname ..'
-//   },
-//   {
-//     type : 'input',
-//     name : 'phone',
-//     message : 'Enter phone number ..'
-//   },
-//   {
-//     type : 'input',
-//     name : 'email',
-//     message : 'Enter email address ..'
-//   }
-// ];
 
-// const questions = {
-//     db_details:[
-//          {
-//     type : 'input',
-//     name : 'host',
-//     message : 'Enter db host: '
-//   }, 
-//   {
-//     type : 'input',
-//     name : 'user',
-//     message : 'Enter db user: '
-//   }, 
-//   {
-//     type : 'input',
-//     name : 'password',
-//     message : 'Enter db user: '
-//   }, 
-//   {
-//     type : 'input',
-//     name : 'password',
-//     message : 'Enter db name: '
-//   },
-//     ]
-// }
+const questions = {
+    db_details: [{
+            type: 'input',
+            name: 'host',
+            message: 'Enter host: '
+        },
+        {
+            type: 'input',
+            name: 'user',
+            message: 'Enter user: '
+        },
+        {
+            type: 'input',
+            name: 'password',
+            message: 'Enter password: '
+        },
+        {
+            type: 'input',
+            name: 'db',
+            message: 'Enter db name: '
+        },
+    ]
+}
 
-// program
-//   .version('0.0.1')
-//   .description('contact management system')
+program
+    .version('0.0.1')
+    .description('Create migration files from db (adonis), for demonstration only!')
 
-// program
-//   .command('getSchema')
-//   .alias('gs')
-//   .description('print db schema')
-//   .action(() => {
-//     prompt(questions).then((answers) =>
-//       addContact(answers));
-//   });
+program
+    .command('createMigration')
+    .alias('c')
+    .description('create migration files from given db details')
+    .action(() => {
+        prompt(questions.db_details).then((con) =>
+            createMigration(con)
+        );
+    });
 
-// program
-//   .command('getContact <name>')
-//   .alias('r')
-//   .description('Get contact')
-//   .action(name => getContact(name));
-
-// program
-//   .command('updateContact <_id>')
-//   .alias('u')
-//   .description('Update contact')
-//   .action(_id => {
-//     prompt(questions).then((answers) =>
-//       updateContact(_id, answers));
-//   });
-
-// program
-//   .command('deleteContact <_id>')
-//   .alias('d')
-//   .description('Delete contact')
-//   .action(_id => deleteContact(_id));
-
-// program
-//   .command('getContactList')
-//   .alias('l')
-//   .description('List contacts')
-//   .action(() => getContactList());
 
 // Assert that a VALID command is provided 
-// if (!process.argv.slice(2).length || !/[arudl]/.test(process.argv.slice(2))) {
-//   program.outputHelp();
-//   process.exit();
-// }
-// program.parse(process.argv)
+if (!process.argv.slice(2).length || !/[arudl]/.test(process.argv.slice(2))) {
+    program.outputHelp();
+    process.exit();
+}
+program.parse(process.argv)
